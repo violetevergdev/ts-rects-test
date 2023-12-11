472ab1d5c925a94758f08146a25a1fe3a2345a6e
Violet Belyaeva <belabelllil@gmail.com>
2023-12-11 22:14:40 +0300

    add error handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import Product from "./components/Product.tsx";
 import { IProduct } from "./models.ts";
 import { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 function App() {
   const [products, setProducts] = useState<IProduct[]>([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   async function fetchProducts() {
-    setLoading(true);
-    const responce = await axios.get<IProduct[]>(
-      "https://fakestoreapi.com/products",
-    );
-    setProducts(responce.data);
-    setLoading(false);
+    try {
+      setError("");
+      setLoading(true);
+      const responce = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products",
+      );
+      setProducts(responce.data);
+      setLoading(false);
+    } catch (e: unknown) {
+      setLoading(false);
+      const error = e as AxiosError;
+      setError(error.message);
+    }
   }
 
   useEffect(() => {
@@ -23,6 +31,8 @@ function App() {
     <div className="container mx-auto max-w-2xl pt-5">
       {loading ? (
         <p className="text-center">Loading...</p>
+      ) : error ? (
+        <p className="text-center text-red-400">{error}</p>
       ) : (
         products?.map((product) => {
           return <Product key={product.id} product={product} />;

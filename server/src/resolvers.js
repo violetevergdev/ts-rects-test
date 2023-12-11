8766efc50bdc09b06913e4ee637a0121230edd50
Violet Belyaeva <belabelllil@gmail.com>
2023-12-11 21:58:47 +0300

    add branch axios

--- server/src/resolvers.js
+++
@@ -1,9 +0,0 @@
-const resolvers = {
-  Query: {
-    products: (_, __, { dataSources }) => {
-      return dataSources.productsAPI.getProducts();
-    },
-  },
-};
-
-export default resolvers;
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { gql, useQuery } from "@apollo/client";
 import Product from "./components/Product.tsx";
 import { IProduct } from "./models.ts";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const PRODUCTS = gql`
-  query ExampleQuery {
-    products {
-      id
-      title
-      price
-      description
-      category
-      image
-      rating {
-        rate
-        count
-      }
-    }
-  }
-`;
 function App() {
-  const { loading, error, data } = useQuery(PRODUCTS);
-  console.log(loading);
-  if (loading) return "Loading...";
-  if (error) return `Error! ${error.message}`;
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [loading, setLoading] = useState(false);
 
+  async function fetchProducts() {
+    setLoading(true);
+    const responce = await axios.get<IProduct[]>(
+      "https://fakestoreapi.com/products",
+    );
+    setProducts(responce.data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {data?.products?.map((product: IProduct) => {
-        return <Product key={product.id} product={product} />;
-      })}
+      {loading ? (
+        <p className="text-center">Loading...</p>
+      ) : (
+        products?.map((product) => {
+          return <Product key={product.id} product={product} />;
+        })
+      )}
     </div>
   );
 }
